7ff5f68837ad3cbf4570cfbede9f70a36bd55783
VGT <tomek@vgtworld.pl>
2016-01-20 17:17:50 +0100

    Add list of products added to receipt.

--- src/main/java/pl/vgtworld/budget/app/receiptcreate/dto/NewReceiptForm.java
+++ src/main/java/pl/vgtworld/budget/app/receiptcreate/dto/NewReceiptForm.java
@@ -1,8 +1,12 @@
 package pl.vgtworld.budget.app.receiptcreate.dto;
 
+import java.util.List;
+
 public class NewReceiptForm {
 
 	private ReceiptStore store;
+
+	private List<ReceiptProduct> products;
 
 	public ReceiptStore getStore() {
 		return store;
@@ -10,6 +14,14 @@ public class NewReceiptForm {
 
 	public void setStore(ReceiptStore store) {
 		this.store = store;
+	}
+
+	public List<ReceiptProduct> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<ReceiptProduct> products) {
+		this.products = products;
 	}
 
 }
---
+++ src/main/java/pl/vgtworld/budget/app/receiptcreate/dto/ReceiptProduct.java
@@ -0,0 +1,52 @@
+package pl.vgtworld.budget.app.receiptcreate.dto;
+
+public class ReceiptProduct {
+
+	private Integer id;
+
+	private String name;
+
+	private Double pricePerUnit;
+
+	private Double amount;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setPricePerUnit(Double pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Double getTotalPrice() {
+		if (amount != null && pricePerUnit != null) {
+			return amount * pricePerUnit;
+		}
+		return 0.0;
+	}
+
+}
