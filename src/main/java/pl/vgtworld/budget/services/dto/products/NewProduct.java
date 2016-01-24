bb8017d3ae83eaf50a03d546c1606a64a8293836
VGT <tomek@vgtworld.pl>
2016-01-24 12:08:09 +0100

    Remove unnecessary tags from NewProduct dto.

--- src/main/java/pl/vgtworld/budget/services/dto/products/NewProduct.java
+++ src/main/java/pl/vgtworld/budget/services/dto/products/NewProduct.java
@@ -1,13 +1,8 @@
 package pl.vgtworld.budget.services.dto.products;
 
-import java.util.List;
-
 public class NewProduct {
 
 	private String name;
-
-	//TODO Remove, when products CRUD is removed from creating receipt page.
-	private List<String> tags;
 
 	public String getName() {
 		return name;
@@ -15,14 +10,6 @@ public class NewProduct {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
 	}
 
 	@Override
