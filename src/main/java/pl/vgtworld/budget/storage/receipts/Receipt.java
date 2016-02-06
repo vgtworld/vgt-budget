b3973723443c30c3428e2f1f5d196496a9ac9188
VGT <tomek@vgtworld.pl>
2016-02-06 15:14:46 +0100

    Create entity and dao for receipts.

---
+++ src/main/java/pl/vgtworld/budget/storage/receipts/Receipt.java
@@ -0,0 +1,74 @@
+package pl.vgtworld.budget.storage.receipts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "receipts")
+public class Receipt {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "store_id", nullable = false)
+	private Integer storeId;
+
+	@Column(name = "purchase_date")
+	@Temporal(TemporalType.DATE)
+	private Date purchaseDate;
+
+	@Column(name = "total_amount")
+	private Double totalAmount;
+
+	@Column(name = "created_at")
+	private Date createdAt;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(Integer storeId) {
+		this.storeId = storeId;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
---
+++ src/main/java/pl/vgtworld/budget/storage/receipts/ReceiptDao.java
@@ -0,0 +1,17 @@
+package pl.vgtworld.budget.storage.receipts;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class ReceiptDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public int create(Receipt receipt) {
+		em.persist(receipt);
+		return receipt.getId();
+	}
+}
