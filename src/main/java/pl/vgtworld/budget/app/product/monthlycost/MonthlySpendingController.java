9237ea73a5d1062eb9ec75188fc419d0a4a165f8
VGT <tomek@vgtworld.pl>
2016-03-28 19:21:53 +0200

    Display month and year in page header.

--- src/main/java/pl/vgtworld/budget/app/product/monthlycost/MonthlySpendingController.java
+++ src/main/java/pl/vgtworld/budget/app/product/monthlycost/MonthlySpendingController.java
@@ -4,17 +4,23 @@ import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Named
 @RequestScoped
 public class MonthlySpendingController {
 
+	private Calendar calendar = Calendar.getInstance();
+
 	@EJB
 	private ProductWithSpendingRepository productWithSpendingRepository;
 
+	public Date getTimestamp() {
+		return calendar.getTime();
+	}
+
 	public List<ProductWithSpendingDto> listProductsWithMostSpending() {
-		Calendar calendar = Calendar.getInstance();
 		int month = calendar.get(Calendar.MONTH);
 		int year = calendar.get(Calendar.YEAR);
 		return productWithSpendingRepository.listProductsWithBiggestSpending(year, month, null);
